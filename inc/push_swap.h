12aca319772c18ce1415e56829bb7d32062e638c
jaay13 <contact@jason-koch.com>
2026-06-05 15:37:06 +0200

    fix: update header comments for push_swap.h and clarify stack structure description

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 14:53:51 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 15:35:08 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/05 15:36:48 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ typedef struct s_node // Single Node
 	struct s_node *prev; // Pointer to previous node
 }	t_node;
 
-typedef struct s_stack // Our stacks a and b, collection of nodes
+typedef struct s_stack // A stack (a or b) made of linked nodes
 {
 	char name;      // a or b
 	int size;       // How many nodes are inside
