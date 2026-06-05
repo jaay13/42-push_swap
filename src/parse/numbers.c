839990f54c792c2b5c51f71eca0e5ccacf178810
jaay13 <contact@jason-koch.com>
2026-06-05 19:33:56 +0200

    fix: correct function call to ft_split in parse_numbers for single argument handling

--- src/parse/numbers.c
+++ src/parse/numbers.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 17:14:27 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 19:27:41 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/05 19:31:29 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char **parse_numbers(int argc, char **argv, int start_index)
 	if (start_index >= argc) // no numeric args after start_index
 		return (NULL);
 	if (arg_count == 1) // one numeric argv entry
-		return (split(argv[start_index], ' '));
+		return (ft_split(argv[start_index], ' '));
 	if (arg_count > 1) // more than one numberic argv entry
 	{
 		tokens = malloc((arg_count + 1) * sizeof(char *));
