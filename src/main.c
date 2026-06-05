d53e2c39f83d16a8bff6627640708f9038ce52c4
jaay13 <contact@jason-koch.com>
2026-06-05 17:00:06 +0200

    feat: implement main function skeleton

--- src/main.c
+++ src/main.c
@@ -6,9 +6,30 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 16:03:29 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 16:09:13 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/05 16:52:27 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+int main(int argc, char **argv)
+{
+	t_stack a;  // Create stacks
+	t_stack b;
+
+	if (argc == 1)
+		return (1);
+	stack_init(&a, 'a'); // initialize stack a
+	stack_init(&b, 'b'); // init stack b
+	parse_input(argc, argv); // parse to check wether flags are present, string needs to be split, checking correct input, range and dupes
+	build_stack(&a, ); // build stack a
+	is_sorted(&a); // check if sorted
+	compute_disorder(&a);	// compute disorder
+	stragety_picker();	// choose either forced strategy or default
+	sort_with_algo(&a, &b, );	// run algo
+	// if (bench flag is present)
+		//print_bench();	// print bench
+	free_stack(&a);	// free everything
+	free_stack(&b);
+	return (0);
+}
