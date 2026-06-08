410a05e669d33617580c815ba41e47b173bffe87
jaay13 <contact@jason-koch.com>
2026-06-08 12:37:56 +0200

    refactor: simplify validate_nums function and set token_count as TODO, remove ft_atol fucntion

--- src/parse/validation.c
+++ src/parse/validation.c
@@ -6,59 +6,43 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 19:35:00 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 20:17:20 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/08 12:35:54 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // check the created char ** for any invalid formats, INT_MAX / INT_MIN, RANGE, DUPES
 #include "../inc/push_swap.h"
 
-int	validate_nums(char **tokens, int count)
+int token_count(char **tokens) //TODO
+{
+	int count;
+
+	count = 0;
+
+	return (count);
+}
+
+bool	validate_nums(char **tokens)
 {
 	long	nbr;
 	int		i;
-	int *nums;
-	int num_used;
+	int		*nums;
 
+	i = token_count(tokens);
+	nums = malloc(sizeof(int) * i);
 	i = 0;
-	nums = malloc(sizeof(int) * count);
 	while (tokens[i])
 	{
 		if (syntax_error(tokens[i])) // check syntax
-			error_n_free(tokens, i);
-		nbr = ft_atol(tokens[i]); // converto to long
+			return (error_n_free(tokens, nums), false);
+		nbr = ft_atol(tokens[i]);           // converto to long
 		if (nbr > INT_MAX || nbr < INT_MIN) // check range
-			error_n_free(tokens, i);
+			return (error_n_free(tokens, nums), false);
 		if (repetition_error(nbr, nums, i)) // check dupes against already accepted nums
-		{
-			error_n_free(tokens, i);
-			nums[i] = (int)nbr; // store accepted num in nums
-		}
+			return (error_n_free(tokens, nums), false);
+		nums[i] = (int)nbr; // store accepted num in nums
 		i++;
 	}
-}
-
-static long	ft_atol(const char *str)
-{
-	int		i;
-	int		symbol;
-	long	result;
-
-	i = 0;
-	symbol = 1;
-	result = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			symbol = -symbol;
-		i++;
-	}
-	while (ft_isdigit(str[i]))
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (result * symbol);
+	free(nums);
+	return (true);
 }
