4b8d5c35816b6eaefe2ed665e8bf6168c7b6ceb8
jaay13 <contact@jason-koch.com>
2026-06-08 12:37:08 +0200

    refactor: remove unused free_str_array function and simplify validation in parse_numbers

--- src/parse/numbers.c
+++ src/parse/numbers.c
@@ -6,25 +6,13 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 17:14:27 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 20:17:03 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/08 11:56:45 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
 // Make sure ./push_swap 1 2 3 and ./push_swap "1 2 3" behave the same
-
-static void free_str_array(char **tokens, int count)
-{
-	int i;
-
-	i = 0;
-	if (!tokens)
-		return ;
-	while (i < count)
-		free(tokens[i++]);
-	free(tokens);
-}
 
 char **parse_numbers(int argc, char **argv, int start_index)
 {
@@ -42,9 +30,8 @@ char **parse_numbers(int argc, char **argv, int start_index)
 		tokens = ft_split(argv[start_index], ' ');
 		if (!tokens)
 			return (NULL);
-		count = token_count(tokens);
-		if (!validate_nums(tokens, count))
-			return (free_str_array(tokens, count), NULL);
+		if (!validate_nums(tokens))
+			return (NULL);
 		return (tokens);
 	}
 	if (arg_count > 1) // more than one numberic argv entry
@@ -56,11 +43,12 @@ char **parse_numbers(int argc, char **argv, int start_index)
 		{
 			tokens[i] = ft_strdup(argv[start_index + i]); // duplicate the argument string into tokens
 			if (!tokens[i]) // if one copy fails, free up what was already allocated and return NULL
-				return (free_str_array(tokens, i), NULL); 
+				return (free_str_array(tokens), NULL); 
 			i++;
 		}
 		tokens[arg_count] = NULL; // set last token to NULL
-		validate_nums(tokens, i);
+		if (!validate_nums(tokens))
+			return (NULL);
 		return (tokens);
 	}
 	return (NULL);		
