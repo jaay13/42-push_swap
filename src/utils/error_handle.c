44f013274e87bc49bcb5407d5194de17fa7fb726
jaay13 <contact@jason-koch.com>
2026-06-08 15:01:11 +0200

    feat: add syntax_error and repetition_error functions

--- src/utils/error_handle.c
+++ src/utils/error_handle.c
@@ -6,15 +6,15 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 11:21:54 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/08 11:57:38 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/08 14:57:45 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void free_str_array(char **tokens)
+void	free_str_array(char **tokens)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!tokens)
@@ -24,9 +24,43 @@ void free_str_array(char **tokens)
 	free(tokens);
 }
 
-void error_n_free(char **tokens, int *nums)
+void	error_n_free(char **tokens, int *nums)
 {
 	if (nums)
 		free(nums);
 	free_str_array(tokens);
+}
+
+int syntax_error(char *token)
+{
+    int i;
+
+    i = 0;
+    if (!(ft_isdigit(token[i]) || token[i] == '-' || token[i] == '+'))
+        return (1);
+    if ((token[i] == '+' || token[i] == '-') && !ft_isdigit(token[i + 1]))
+        return (1);
+    i++;
+    while (token[i])
+    {
+        if (!ft_isdigit(token[i]))
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int repetition_error(long nbr, int *nums, int i)
+{
+	int j;
+
+	j = 0;
+
+	while (j < i)
+	{
+		if (nums[j] == nbr)
+			return (1);
+		j++;
+	}
+	return (0);
 }
