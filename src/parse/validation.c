200c32bfd8cf348610174368dc597ed10791c8fe
jaay13 <contact@jason-koch.com>
2026-06-08 15:31:12 +0200

    refactor: add empty string guard, format code for consistency and improve readability in validation functions

--- src/parse/validation.c
+++ src/parse/validation.c
@@ -6,16 +6,16 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 19:35:00 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/08 15:06:54 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/08 15:30:22 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // check the created **tokens for any invalid formats, INT_MAX / INT_MIN, RANGE, DUPES
 #include "../inc/push_swap.h"
 
-static int token_count(char **tokens) 
+static int	token_count(char **tokens)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	while (tokens[count])
@@ -30,20 +30,22 @@ bool	validate_nums(char **tokens)
 	int		*nums;
 
 	i = token_count(tokens);
+	if (i == 0) 												// filters out empty string ""
+		return (false);
 	nums = malloc(sizeof(int) * i);
 	if (!nums)
 		return (false);
 	i = 0;
-	while (tokens[i])
+	while (tokens[i]) 											// loops all tokens are checked
 	{
-		if (syntax_error(tokens[i])) // check syntax
+		if (syntax_error(tokens[i])) 							// check syntax
 			return (error_n_free(tokens, nums), false);
-		nbr = ft_atol(tokens[i]);           // converto to long
-		if (nbr > INT_MAX || nbr < INT_MIN) // check range
+		nbr = ft_atol(tokens[i]);          						// converts to long, which prevents gargabe values
+		if (nbr > INT_MAX || nbr < INT_MIN) 					// check range
 			return (error_n_free(tokens, nums), false);
-		if (repetition_error(nbr, nums, i)) // check dupes against already accepted nums
+		if (repetition_error(nbr, nums, i)) 					// check dupes against already accepted nums
 			return (error_n_free(tokens, nums), false);
-		nums[i] = (int)nbr; // store accepted num in nums
+		nums[i] = (int)nbr; 									// store accepted nbr in nums
 		i++;
 	}
 	free(nums);
