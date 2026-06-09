9503e0da9cecb95ee13e3ce1af2501f19107a869
jaay13 <contact@jason-koch.com>
2026-06-09 22:43:52 +0200

    fix: correct function calls in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 16:03:29 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 18:04:23 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/09 22:43:23 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 int main(int argc, char **argv)
 {
-	t_stack a;  // Create stacks
+	t_stack a;  								// Create stacks
 	t_stack b;
 	t_config config;
 
 	if (argc == 1)
-		return (1);
-	stack_init(&a, 'a'); // initialize stack a
-	stack_init(&b, 'b'); // init stack b
-	parse_input(argc, argv, &config); // parse to check wether flags are present, string needs to be split, checking correct input, range and dupes
-	build_stack(&a, ); // build stack a
-	is_sorted(&a); // check if sorted
-	compute_disorder(&a);	// compute disorder
-	stragety_picker();	// choose either forced strategy or default
-	sort_with_algo(&a, &b, );	// run algo
+		return (0); 							// no parameters 
+	stack_init(&a, 'a'); 						// initialize stack a
+	stack_init(&b, 'b'); 						// init stack b
+	parse_input(argc, argv, &config); 			// parse to check wether flags are present, string needs to be split, checking correct input, range and dupes
+	build_stack(&a, &config); 					// build stack a
+	compute_disorder(&a, &config);				// compute disorder, check if sorted (disorder 0)
+	stragety_picker(&config);					// choose either forced strategy or adaptive default based on disorder
+	sort_with_algo(&a, &b, &config);			// run algo
 	// if (bench flag is present)
 		//print_bench();	// print bench
 	free_stack(&a);	// free everything
