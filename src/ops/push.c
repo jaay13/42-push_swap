b4dd7d693f976609a8fd81d965433db591d34a19
jaay13 <contact@jason-koch.com>
2026-06-09 14:12:45 +0200

    refactor: improve readability and comments in push function for stack operations

--- src/ops/push.c
+++ src/ops/push.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 17:49:57 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/08 18:17:50 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/09 14:12:31 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,25 +40,25 @@ static void	push(t_stack *src, t_stack *dst)
 {
 	t_node	*node_to_be_pushed;
 
-	if (!src->top)
+	if (!src->top)											// do nothing if empty
 		return ;
-	node_to_be_pushed = src->top;
-	src->top = src->top->next;
-	if (src->top)
-		src->top->prev = NULL;
-	else
-		src->bottom = NULL;
+	node_to_be_pushed = src->top; 							// store node to push
+	src->top = src->top->next;								// set new src stack top
+	if (src->top)											
+		src->top->prev = NULL;								// clear prev of new top
+	else													
+		src->bottom = NULL;									// mark src to empty
 	node_to_be_pushed->prev = NULL;
-	if (!dst->top)
+	if (!dst->top)											// if dst empty
 	{
-		dst->top = node_to_be_pushed;
-		dst->bottom = node_to_be_pushed;
-		node_to_be_pushed->next = NULL;
+		dst->top = node_to_be_pushed;						// set to new top
+		dst->bottom = node_to_be_pushed;					// set to new bottom
+		node_to_be_pushed->next = NULL;						
 	}
 	else
 	{
-		node_to_be_pushed->next = dst->top;
-		node_to_be_pushed->next->prev = node_to_be_pushed;
-		dst->top = node_to_be_pushed;
+		node_to_be_pushed->next = dst->top;					// Link new top to old 
+		node_to_be_pushed->next->prev = node_to_be_pushed; 	// Link old top to new top
+		dst->top = node_to_be_pushed;						// update top
 	}
 }
