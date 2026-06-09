00bf40180dd7899e52248a2dbeb25493ae0d6639
jaay13 <contact@jason-koch.com>
2026-06-09 15:30:43 +0200

    refactor: enhance input parsing by initializing tokens and count in config

--- src/parse/input.c
+++ src/parse/input.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 17:28:46 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/08 15:52:29 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/09 15:29:53 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 static void config_init(t_config *config)
 {
 	config->num_start = 1;
-	config->strategy = ADAPTIVE; 							// set adaptive as default strat
+	config->strategy = ADAPTIVE; 										// set adaptive as default strat
 	config->bench = false;
+	config->tokens = NULL;
+	config->count = 0;
 }
 
 void	parse_input(int argc, char **argv, t_config *config)
@@ -25,17 +27,22 @@ void	parse_input(int argc, char **argv, t_config *config)
 
 	i = 1;
 	config_init(config);
-	while (i < argc) 										// Loop until argv arguments are numeric
+	while (i < argc) 													// Loop until argv arguments are numeric
 	{
-		if (parse_flags(argv[i], config)) 					// check wether argument is a flag and store it
+		if (parse_flags(argv[i], config)) 								// check wether argument is a flag and store it
 			i++;
 		else
 			break ;
 	}
-	config->num_start = i; 									// set numeric start index
-	if (!parse_numbers(argc, argv, config->num_start)) 		// Checks parse_numbers and handles error incase it returns NULL on error
+	config->num_start = i; 												// set numeric start index
+	config->tokens = parse_numbers(argc, argv, config->num_start);		// stores **tokens from parse_numbers
+	if (!config->tokens) 												// Checks parse_numbers and handles error in case it returns NULL on error
 	{
 		write(2, "Error\n", 6);
 		exit(1);
 	}
+	i = 0;																// reset i as it is now used as a counter
+	while (config->tokens[i])											// count the amount of tokens created
+		i++;
+	config->count = i;
 }
