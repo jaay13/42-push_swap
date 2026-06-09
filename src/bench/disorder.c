e918c19c8cc63b66b98c60beefa196acf9d93f05
jaay13 <contact@jason-koch.com>
2026-06-09 22:44:32 +0200

    feat: implement compute_disorder function to evaluate stack disorder

---
+++ src/bench/disorder.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   disorder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 22:10:35 by jakoch            #+#    #+#             */
+/*   Updated: 2026/06/09 22:42:30 by jakoch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	compute_disorder(t_stack *a, t_config *config)
+{
+	int mistakes;
+	int total_pairs;
+	t_node *node1;
+	t_node *node2;
+
+	mistakes = 0;
+	total_pairs = 0;
+	node1 = a->top;
+	while(node1)
+	{
+		node2 = node1->next;
+		while(node2)
+		{
+			total_pairs++;
+			if (node1->value > node2->value)
+				mistakes++;
+			node2 = node2->next;
+		}
+		node1 = node1->next;
+	}
+	config->disorder = (float)mistakes/ total_pairs;
+	if (config->disorder == 0.0f)							// check if disorder 0 (fully sorted), exit
+		exit(0);
+}
