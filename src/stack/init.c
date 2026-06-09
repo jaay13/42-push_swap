9e49fd7b6b4e10a844ee3467d186dc2c9c0092e1
jaay13 <contact@jason-koch.com>
2026-06-09 22:36:09 +0200

    feat: add build_stack function, refactor: rename stack_init pointer to t_stack *stack for clarity

--- src/stack/init.c
+++ src/stack/init.c
@@ -6,18 +6,39 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 16:59:18 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/05 17:09:11 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/09 16:51:21 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	stack_init(t_stack *ptr, char name)
+void	stack_init(t_stack *stack, char name)
 {
-	if (!ptr)
+	if (!stack)
 		return ;
-	ptr->name = name;
-	ptr->size = 0;
-	ptr->top = NULL;
-	ptr->bottom = NULL;
+	stack->name = name;
+	stack->size = 0;
+	stack->top = NULL;
+	stack->bottom = NULL;
+}
+
+void	build_stack(t_stack *stack, t_config *config)
+{
+	t_node *new_node;
+	int i;
+	int nbr;
+
+	i = 0;
+	while (config->tokens[i])
+	{
+		new_node = malloc(sizeof(t_node));
+		if (!new_node)
+			return ;
+		nbr = ft_atoi(config->tokens[i]);
+		new_node->value = nbr;
+		append_node(stack, new_node);
+		i++;
+		stack->size++;
+	}
+	free(new_node);
 }
