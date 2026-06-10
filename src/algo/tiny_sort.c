cb72046d5b007ca6eb6edfe79e97e9d5b20cf96a
jaay13 <contact@jason-koch.com>
2026-06-10 18:27:42 +0200

    refactor: move bring_min_to_top_of from tiny_sort.c to stack_utils and remove the static keyword to use it in simple_sort

--- src/algo/tiny_sort.c
+++ src/algo/tiny_sort.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 16:17:43 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 17:58:39 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 18:25:51 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,34 +49,6 @@ static void	sort_three(t_stack *a)
 		if (a->top->value > a->top->next->value)	 	// updated top > middle
 			sa(a);
 	}
-}
-static void	bring_min_to_top_of(t_stack *a)
-{
-	t_node *current;
-	int min_value;
-	int min_value_index;
-	int i;
-
-	current = a->top;									
-	min_value = current->value;
-	min_value_index = 0;
-	i = 0;
-	while (current)										// walk the nodes in stack a
-	{
-		if (current->value < min_value)					// update min value and index
-		{
-			min_value = current->value;
-			min_value_index = i;
-		}
-		i++;
-		current = current->next;						// go onto the next node
-	}
-	if (min_value_index <= a->size / 2)					// is the min value in the top half or bottom half?  a->size gives total nodes / 2 gives the middle, anything below that = closer to the top
-		while (a->top->value != min_value)					
-			ra(a);										// ra as min value closer to the top
-	else
-		while (a->top->value != min_value)					
-			rra(a);										// rra as min value is closer to the bottom
 }
 
 static void	sort_four_five(t_stack *a, t_stack *b)
--- src/stack/stack_utils.c
+++ src/stack/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 16:21:57 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/09 16:48:18 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 18:26:07 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,33 @@ void	append_node(t_stack *stack, t_node *node)
 		node->prev = stack->bottom;
 		stack->bottom = node;
 	}
+}
+
+void	bring_min_to_top_of(t_stack *a)
+{
+	t_node *current;
+	int min_value;
+	int min_value_index;
+	int i;
+
+	current = a->top;									
+	min_value = current->value;
+	min_value_index = 0;
+	i = 0;
+	while (current)										// walk the nodes in stack a
+	{
+		if (current->value < min_value)					// update min value and index
+		{
+			min_value = current->value;
+			min_value_index = i;
+		}
+		i++;
+		current = current->next;						// go onto the next node
+	}
+	if (min_value_index <= a->size / 2)					// is the min value in the top half or bottom half?  a->size gives total nodes / 2 gives the middle, anything below that = closer to the top
+		while (a->top->value != min_value)					
+			ra(a);										// ra as min value closer to the top
+	else
+		while (a->top->value != min_value)					
+			rra(a);										// rra as min value is closer to the bottom
 }
