92897b4638a448164280867bbb9ae3fc34b29eab
jaay13 <contact@jason-koch.com>
2026-06-10 17:50:16 +0200

    refactor: change second if statement to else if in choose_algo

--- src/algo/choose_algo.c
+++ src/algo/choose_algo.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 13:04:18 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 14:09:27 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 17:47:23 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ void	choose_algo(t_stack *a, t_stack *b, t_config *config)
 {
 	if (a->size <= 5)
 		tiny_sort(a, b);
-	if (config->strategy == SIMPLE)
+	else if (config->strategy == SIMPLE)
 		simple_sort(a, b);
 	else if (config->strategy == MEDIUM)
 		medium_sort(a, b);
 	else
 		complex_sort(a, b);
 }
-
