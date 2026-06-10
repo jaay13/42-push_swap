992fd55ef20fc250b2ab925308c4a5f297420253
jaay13 <contact@jason-koch.com>
2026-06-10 13:53:11 +0200

    update: fix comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 16:03:29 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/09 22:45:02 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 12:56:36 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	parse_input(argc, argv, &config); 			// parse to check wether flags are present, string needs to be split, checking correct input, range and dupes
 	build_stack(&a, &config); 					// build stack a
 	compute_disorder(&a, &config);				// compute disorder, check if sorted (disorder 0)
-	strategy_picker(&config);					// choose either forced strategy or adaptive default based on disorder
+	strategy_picker(&config);					// set either forced strategy or adaptive default based on disorder
 	sort_with_algo(&a, &b, &config);			// run algo
 	// if (bench flag is present)
 		//print_bench();	// print bench
