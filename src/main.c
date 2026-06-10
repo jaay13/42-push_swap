2cfec03b10cf68f06b0c74898d80558693b3d73a
jaay13 <contact@jason-koch.com>
2026-06-10 14:13:01 +0200

    refactor: rename function sort_with_algo to choose_algo in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 16:03:29 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 12:56:36 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 14:09:16 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 	build_stack(&a, &config); 					// build stack a
 	compute_disorder(&a, &config);				// compute disorder, check if sorted (disorder 0)
 	strategy_picker(&config);					// set either forced strategy or adaptive default based on disorder
-	sort_with_algo(&a, &b, &config);			// run algo
+	choose_algo(&a, &b, &config);				// run algo
 	// if (bench flag is present)
 		//print_bench();	// print bench
 	free_stack(&a);	// free everything
