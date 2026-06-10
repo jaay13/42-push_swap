f7919250aaced6d3344a05cc54258d95b6d6042d
jaay13 <contact@jason-koch.com>
2026-06-10 18:48:38 +0200

    feat: add simple_sort declaration in header

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 14:53:51 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 18:42:17 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 18:45:53 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,8 @@ bool	validate_nums(char **tokens);
 void	choose_algo(t_stack *a, t_stack *b, t_config *config);
 void	strategy_picker(t_config *config);
 void	sort_three(t_stack *a);
+void	simple_sort(t_stack *a, t_stack *b);
+
 
 /* ---------------	OPERATIONS	--------------- */
 void	sa(t_stack *a);
