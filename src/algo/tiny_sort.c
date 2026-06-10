e619a38d0482b362e538f8dd7c2c65eb3afcd8e2
jaay13 <contact@jason-koch.com>
2026-06-10 14:12:02 +0200

    feat: implement tiny_sort dispatcher

--- src/algo/tiny_sort.c
+++ src/algo/tiny_sort.c
@@ -12,3 +12,12 @@
 
 #include "../inc/push_swap.h"
 
+void	tiny_sort(t_stack *a, t_stack *b)
+{
+	if (a->size == 2)
+		sort_two(a);
+	else if (a->size == 3)
+		sort_three(a);
+	else if (a->size == 5)
+		sort_five(a, b);
+}
