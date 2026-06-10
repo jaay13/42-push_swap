ab5f85f166fc55161aae50794a8cd7ddd03244f2
jaay13 <contact@jason-koch.com>
2026-06-10 18:48:14 +0200

    feat: implement simple_sort using min extraction method one baseline algorithm in the O(n2) class

--- src/algo/simple.c
+++ src/algo/simple.c
@@ -12,5 +12,28 @@
 
 #include "../inc/push_swap.h"
 
+/* 
+
+	Min extraction method:
+
+	Look at the current stack a
+	Find the minimum value
+	Rotate a so that value reaches the top
+	Push it to b
+	Repeat until a is small enough to finish directly
+	Sort the remaining tiny core
+	Push everything back
+
+*/
+
 void	simple_sort(t_stack *a, t_stack *b)
-{}
+{
+	while (a->size > 3)
+	{
+		bring_min_to_top_of(a);
+		pb(a, b);								// push smallest value to stack b
+	}
+	sort_three(a);
+	while (b->size > 0)
+		pa(a, b);								// push them back to a
+}
