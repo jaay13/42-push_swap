5683b7954db92d246bb26561579693a8c2e510f3
jaay13 <contact@jason-koch.com>
2026-06-10 20:06:46 +0200

    todo: add comprehensive todo for 11/06

--- src/algo/choose_algo.c
+++ src/algo/choose_algo.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 13:04:18 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 17:47:23 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 20:05:23 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,22 @@ void	choose_algo(t_stack *a, t_stack *b, t_config *config)
 	else
 		complex_sort(a, b);
 }
+
+/*  After checking the task carefully I think using a tiny sort is not what the task
+ 	actually wants as it says: Any selector should work regardless of input size or disorder
+
+	which means for me at least: 
+	--complex should use a complex algorithm even if input is 2, 3, 4, 5
+	same for the others
+	means no tiny_sort rerouting allowed?
+	The user should obviously know it will perform worse, but should have the freedom to choose
+
+	TODO:
+	remove tiny_sort and refactor so its task compliant
+	add special cases in each algorithm to handle these low inputs possible?
+	
+	EXTRA:
+	with this option if using --complex and --bench bench would say used complex algorithm
+	which would be misleading if input is under 5 numbers
+
+*/
