ddf716cae6976184b3f7e8290ba45dbf07fc2a23
jaay13 <contact@jason-koch.com>
2026-06-10 14:02:02 +0200

    refactor: remove config->strategy_picked, change to int is_flag

--- src/parse/flags.c
+++ src/parse/flags.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 17:13:14 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 12:48:35 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/10 13:59:50 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,32 @@
 
 int	parse_flags(char *arg, t_config *config)
 {
+	int is_flag;
+
+	is_flag = 0;
 	if (ft_strncmp(arg, "--simple", 9) == 0 && arg[8] == '\0')	// compare the flag to "--simple" if they match strncmp returns 0, 9 characers because 8 + \0
 	{
 		config->strategy = SIMPLE;
-		config->strategy_picked = true;
+		is_flag = 1;
 	}
 	else if (ft_strncmp(arg, "--medium", 9) == 0 && arg[8] == '\0') // && arg[8] checks also if the last character is '\0' and so not something like "--mediums" will be accepted
 	{
 		config->strategy = MEDIUM;
-		config->strategy_picked = true;
+		is_flag = 1;
 	}
 	else if (ft_strncmp(arg, "--complex", 10) == 0 && arg[9] == '\0')
 	{
 		config->strategy = COMPLEX;
-		config->strategy_picked = true;
+		is_flag = 1;
 	}
 	else if (ft_strncmp(arg, "--adaptive", 11) == 0 && arg[10] == '\0')
 	{
-		config->strategy_picked = true;
+		is_flag = 1;
 	}
 	else if (ft_strncmp(arg, "--bench", 8) == 0 && arg[7] == '\0')
 	{
 		config->bench = true;
-		config->strategy_picked = true;
+		is_flag = 1;
 	}
-	return (config->strategy_picked);
+	return (is_flag);
 }
