ae7461a6cc16378992fd4fe238b63c4056162c0e
jaay13 <contact@jason-koch.com>
2026-06-10 15:06:20 +0200

    feat: implement sort_three functions, remove sort_two as only sa(a) needed

--- src/algo/tiny_sort.c
+++ src/algo/tiny_sort.c
@@ -15,9 +15,39 @@
 void	tiny_sort(t_stack *a, t_stack *b)
 {
 	if (a->size == 2)
-		sort_two(a);
+		sa(a);
 	else if (a->size == 3)
 		sort_three(a);
 	else if (a->size == 5)
 		sort_five(a, b);
+}
+
+static void sort_three(t_stack *a)
+{
+	int top;
+	int middle;
+	int bottom;
+
+	top = a->top->value;
+	middle = a->top->next->value;
+	bottom = a->bottom->value;
+	if (top < bottom && middle < bottom) 			// is bottom the biggest?
+		sa(a);
+	else if (top < middle && middle > bottom) 		// is middle the biggest?
+	{
+		rra(a);
+		if (a->top->value > a->top->next->value) 	// updated top > middle
+			sa(a);
+	}
+	else // top is the biggest
+	{
+		ra(a);
+		if (a->top->value > a->top->next->value) 	// updated top > middle
+			sa(a);
+	}
+}
+
+static void sort_five(t_stack *a, t_stack *b)
+{
+
 }
