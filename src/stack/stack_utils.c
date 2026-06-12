7142e480707dd2bc29677e6990e2e6c39a3eea47
jaay13 <contact@jason-koch.com>
2026-06-12 18:45:43 +0200

    feat: implement bring_max_to_top_of function and use it in push_max_chunks, check maybe further refactor needed

--- src/stack/stack_utils.c
+++ src/stack/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 16:21:57 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/10 18:26:07 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/12 18:43:58 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,43 @@ void	bring_min_to_top_of(t_stack *a)
 	else
 		while (a->top->value != min_value)					
 			rra(a);										// rra as min value is closer to the bottom
+}
+void	bring_max_to_top_of(t_stack *b)
+{
+	t_node	*current;
+	int		max_value;
+	int		max_value_index;
+	int		i;
+
+	if (!b || !b->top)
+		return ;
+	current = b->top;
+	max_value = current->value;
+	max_value_index = 0;
+	i = 0;
+	while (current)
+	{
+		if (current->value > max_value)
+		{
+			max_value = current->value;
+			max_value_index = i;
+		}
+		i++;
+		current = current->next;
+	}
+	if (max_value_index <= b->size / 2)
+		while (b->top->value != max_value)
+			rb(b);
+	else
+		while (b->top->value != max_value)
+			rrb(b);
+}
+
+void	push_max_chunks_to(t_stack *a, t_stack *b)
+{
+	while (b->top)
+	{
+		bring_max_to_top_of(b);
+		pa(a, b);
+	}
 }
