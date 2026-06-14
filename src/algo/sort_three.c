5bfb982b40e9f0ff2d8dee06a9299047501b2e68
jaay13 <contact@jason-koch.com>
2026-06-14 16:13:49 +0200

    refactor: remove tiny_sort function from sort_three.c

--- src/algo/sort_three.c
+++ src/algo/sort_three.c
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tiny_sort.c                                        :+:      :+:    :+:   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 16:17:43 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/13 13:03:43 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/14 16:11:36 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../inc/push_swap.h"
-
-void	sort_three(t_stack *a, t_config *config);
-
-void	tiny_sort(t_stack *a, t_stack *b, t_config *config)
-{
-	if (a->size == 2)
-		sa(a, config);
-	else if (a->size == 3)
-		sort_three(a, config);
-	else if (a->size == 4 || a->size == 5)				// prevents using a medium, complex algo on low stack size
-		simple_sort(a, b, config);
-}
 
 void	sort_three(t_stack *a, t_config *config)
 {
@@ -35,7 +23,7 @@ void	sort_three(t_stack *a, t_config *config)
 	middle = a->top->next->value;
 	bottom = a->bottom->value;
 	if (top < bottom && middle < bottom) 				// is bottom the biggest?
-		sa(a, config);											// does work because the other option e.g 1,2,3 is already filtered out by disorder 0
+		sa(a, config);									// does work because the other option e.g 1,2,3 is already filtered out by disorder 0
 	else if (top < middle && middle > bottom) 			// is middle the biggest?
 	{
 		rra(a, config);
@@ -49,3 +37,4 @@ void	sort_three(t_stack *a, t_config *config)
 			sa(a, config);
 	}
 }
+
