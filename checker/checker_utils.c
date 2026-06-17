6b28692c4e0a43097989abf9b157e305ed0c0546
jaay13 <contact@jason-koch.com>
2026-06-17 14:52:51 +0200

    feat: implement read_input function, use get_next_line to read from stdin, execute with to be created function execute_input_operation

--- checker/checker_utils.c
+++ checker/checker_utils.c
@@ -6,9 +6,21 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 14:05:56 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/17 14:06:03 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/17 14:52:06 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+void	read_input(t_stack *a, t_stack *b, t_config *config)
+{
+	char	*line;
+
+	line = get_next_line(0);									// get input from stdin, eg.: ra, pa, pb
+	while (line)
+	{
+		execute_input_operation(a, b, config, line);			// executes operation command, if no correct input print error
+		free(line);
+		line = get_next_line(0);
+	}
+}
