e3a49502b941309d4778eb2f03315ae97707d148
jaay13 <contact@jason-koch.com>
2026-06-17 17:46:02 +0200

    refactor: remove gnl utils function that are in libft include libft in gnl header, otherwise error of dublicate definition

--- libft/get_next_line/get_next_line_bonus.h
+++ libft/get_next_line/get_next_line_bonus.h
@@ -6,16 +6,16 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:52 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/02 14:36:42 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/17 17:43:21 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stdint.h> // SIZE_MAX
 # include <stdlib.h> // malloc, free
 # include <unistd.h> // read
+# include "../libft.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
@@ -24,9 +24,6 @@
 char	*get_next_line(int fd);
 
 /* ---------------	UTILS	--------------- */
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen_c(const char *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
 #endif
--- libft/get_next_line/get_next_line_utils_bonus.c
+++ libft/get_next_line/get_next_line_utils_bonus.c
@@ -6,42 +6,11 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:49 by jakoch            #+#    #+#             */
-/*   Updated: 2026/06/02 15:47:54 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/06/17 17:43:44 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void			*res;
-	unsigned char	*ptr;
-	size_t			i;
-
-	if (nmemb > 0 && size > SIZE_MAX / nmemb)
-		return (NULL);
-	res = malloc(nmemb * size);
-	if (!res)
-		return (NULL);
-	ptr = res;
-	i = 0;
-	while (i < nmemb * size)
-		ptr[i++] = 0;
-	return (res);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if ((char)c == '\0')
-		return ((char *)s);
-	return (NULL);
-}
 
 size_t	ft_strlen_c(const char *s, char c)
 {
@@ -52,48 +21,5 @@ size_t	ft_strlen_c(const char *s, char c)
 		i++;
 	if (s[i] == c && s[i] != '\0')
 		i++;
-	return (i);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*sub;
-	size_t	s_len;
-	size_t	sub_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen_c(s, '\0');
-	if (start >= s_len)
-	{
-		sub = malloc(1);
-		if (!sub)
-			return (NULL);
-		sub[0] = '\0';
-		return (sub);
-	}
-	sub_len = s_len - start;
-	if (len > sub_len)
-		len = sub_len;
-	sub = malloc(len + 1);
-	if (!sub)
-		return (NULL);
-	ft_strlcpy(sub, s + start, len + 1);
-	return (sub);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-
-	i = 0;
-	if (!dstsize)
-		return (0);
-	while (src[i] && i < (dstsize - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
 	return (i);
 }
